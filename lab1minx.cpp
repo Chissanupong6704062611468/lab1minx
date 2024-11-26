c6d388fa0a9458d860de775bdbeef9a6c2d188b1
6704062611468 <s6704062611468@email.kmutnb.ac.th>
2024-11-26 15:05:54 +0700

    ส่งข้อ2ครับ

--- lab1minx.cpp
+++ lab1minx.cpp
@@ -6,11 +6,15 @@
 int main() {
   int a = 15;
   int b = 20;
+  scanf("%d",&a);
+  scanf("%d",&b);
   
   if (a > b) {
     printf("a is greater than b");
+    //diff of a and b
   }else{
   	 printf("b is greater than a");
+  	 //diff b and a
   }  
   return 0;
 }
