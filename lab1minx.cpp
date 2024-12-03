c67a1962969d32f7573a4ef71c47c4eac7f34724
6704062611468 <s6704062611468@email.kmutnb.ac.th>
2024-12-03 13:14:28 +0700

    ส่งข้อ 4 ครับ

--- lab1minx.cpp
+++ lab1minx.cpp
@@ -11,11 +11,11 @@ int main() {
   
   if (a > b) {
     printf("a is greater than b");
-    printf("\m%d",a-b);
+    printf("\n%d",a-b);
     //diff of a and b
   }else{
   	 printf("b is greater than a");
-  	 printf("%d",b-a);
+  	 printf("\n%d",b-a);
   	 //diff b and a
   }  
   return 0;
